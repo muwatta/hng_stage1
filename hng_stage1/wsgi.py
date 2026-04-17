037dbadf08e0b9b23c17733f39ba81d5dcdb966e
Muwatta <abdullahmusliudeen@gmail.com>
2026-04-17 07:49:50 +0100

    update the database

--- hng_stage1/wsgi.py
+++ hng_stage1/wsgi.py
@@ -1,6 +1,11 @@
+# hng_stage1/wsgi.py
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hng_stage1.settings')
+
+# Run migrations on cold start
+from django.core.management import call_command
+call_command('migrate', '--no-input')
+
 application = get_wsgi_application()
-app = application
